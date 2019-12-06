48858182156b6bea215aec73ccd30903c36e8da7
FaisalAlqarni <47994211+FaisalAlqarni@users.noreply.github.com>
2019-12-06 03:02:01 +0300

    converted from static to dynamic

--- lib/Pages/Categoory.dart
+++ lib/Pages/Categoory.dart
@@ -1,32 +1,36 @@
 import 'package:senior_project/pages/event.dart';
-class Categoory{
+
+class Categoory {
   int id;
   String name;
   List<Event> events;
 
-
-
-
-  Categoory(
+  Categoory({
     this.id,
     this.name,
     this.events,
+  });
+  
+  factory Categoory.fromJson(Map<String, dynamic> json) {
+  var list = json['events'] as List;
+    print(list.runtimeType); //returns List<dynamic>
+    List<Event> eventsList = list.map((i) => Event.fromJson(i)).toList();
 
-  );
-
-  Categoory.fromJson(Map<String, dynamic> json)
+    return new Categoory(
+      id: json['id'],
+      name: json['name'],
+      events: eventsList,
+    );
+  }
+  
+/*   Categoory.fromJson(Map<String, Object> json)
       : id = json['id'],
-        name = json['name'],
-        events = json['events'];
+        name = json['name'];
+        //events = json['events']; */
 
-
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
         'events': events,
-
       };
-
-
 }
